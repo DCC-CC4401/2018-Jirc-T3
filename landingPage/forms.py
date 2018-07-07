7a869e06f8b70007aa0faf1b5afd02a9d33f4483
rs077 <rodrigo.soria@ing.uchile.cl>
2018-07-07 03:32:03 -0400

    requisitos 1-4 listos

--- landingPage/forms.py
+++ landingPage/forms.py
@@ -1,19 +1,28 @@
 from django.contrib.auth.forms import UserCreationForm
-
 from landingPage.models import (Profile, User)
-
+from django import forms
 
 class PersonSignUpForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
+    first_name = forms.CharField(max_length=140, required=True)
+    last_name = forms.CharField(max_length=140, required=True)
+    email = forms.EmailField(required=True)
+    rut = forms.CharField(max_length=20, required=True)
+
+    class Meta:
         model = User
+        fields = ("first_name", "last_name", "rut", "email", "password1", "password2")
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.is_person = True
+        user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.rut = self.cleaned_data["rut"]
+        user.username = user.email
         if commit:
             user.save()
         return user
-
 
 class ManagerSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
--- landingPage/models.py
+++ landingPage/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class User(AbstractUser):
     is_person = models.BooleanField(default=False)
     is_manager = models.BooleanField(default=False)
+    rut = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=80, verbose_name='Email', unique=True)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
